5478f7bf86d0f09c3a2846be06b3b0dc0e8f070f
Txuritan <txuritan@protonmail.com>
2020-06-17 20:29:23 -0400

    I forgot to do chapter

--- stry-backend-sqlite/src/chapter/mod.rs
+++ stry-backend-sqlite/src/chapter/mod.rs
@@ -60,41 +60,9 @@ impl BackendChapter for SqliteBackend {
                 let conn = inner.0.get()?;
 
                 let row = conn
-                    .query_row_anyhow(
+                    .type_query_row_anyhow::<Chapter, _>(
                         include_str!("get-item.sql"),
                         rusqlite::params![story_id, chapter_number],
-                        |row| {
-                            Ok(Chapter {
-                                id: row
-                                    .get(0)
-                                    .context("Attempting to get row index 0 for chapter")?,
-
-                                name: row
-                                    .get(1)
-                                    .context("Attempting to get row index 1 for chapter")?,
-
-                                pre: row
-                                    .get(2)
-                                    .context("Attempting to get row index 2 for chapter")?,
-                                main: row
-                                    .get(3)
-                                    .context("Attempting to get row index 3 for chapter")?,
-                                post: row
-                                    .get(4)
-                                    .context("Attempting to get row index 4 for chapter")?,
-
-                                words: row
-                                    .get(5)
-                                    .context("Attempting to get row index 5 for chapter")?,
-
-                                created: row
-                                    .get(6)
-                                    .context("Attempting to get row index 6 for chapter")?,
-                                updated: row
-                                    .get(7)
-                                    .context("Attempting to get row index 7 for chapter")?,
-                            })
-                        },
                     )
                     .context("Unable to get story chapter")?;
 

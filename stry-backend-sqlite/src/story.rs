f5f7acb23c424c5d09c1e3bbcd0bdfdf30050a4a
Txuritan <txuritan@protonmail.com>
2020-06-17 23:46:33 -0400

    Going to add tracing to the database backends
    For some reason its breaking `include_str`, thinks thats its in the `src` folder and not the one its actually in

--- stry-backend-sqlite/src/story.rs
+++ stry-backend-sqlite/src/story.rs
@@ -18,6 +18,7 @@ use {
 
 #[async_trait::async_trait]
 impl BackendStory for SqliteBackend {
+    #[tracing::instrument]
     async fn all_stories(&self, offset: u32, limit: u32) -> anyhow::Result<Option<List<Story>>> {
         let ids = match tokio::task::spawn_blocking({
             let inner = self.clone();
@@ -81,6 +82,7 @@ impl BackendStory for SqliteBackend {
         Ok(Some(List { total, items }))
     }
 
+    #[tracing::instrument]
     async fn get_story(&self, id: Cow<'static, str>) -> anyhow::Result<Option<Story>> {
         let story_part = match tokio::task::spawn_blocking({
             let inner = self.clone();
@@ -265,6 +267,7 @@ impl BackendStory for SqliteBackend {
         Ok(Some(story))
     }
 
+    #[tracing::instrument]
     async fn search_stories(
         &self,
         _input: Cow<'static, str>,

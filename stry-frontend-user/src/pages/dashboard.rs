0d33f65751b58e3d3e2c471cdc9f730b97a9c348
Txuritan <txuritan@protonmail.com>
2020-07-07 02:19:09 -0400

    Added blank templates and askama pages

--- stry-frontend-user/src/pages/dashboard.rs
+++ stry-frontend-user/src/pages/dashboard.rs
@@ -1,11 +1,46 @@
 use askama::Template;
 
 #[derive(Template)]
-#[template(path = "dashboard/downloads.html")]
-struct Downloads {
+#[template(path = "dashboard/about.html")]
+pub struct About {
     version: &'static str,
     git: &'static str,
 
     title: String,
-    search: Option<String>,
+}
+
+#[derive(Template)]
+#[template(path = "dashboard/database.html")]
+pub struct Database {
+    version: &'static str,
+    git: &'static str,
+
+    title: String,
+}
+
+#[derive(Template)]
+#[template(path = "dashboard/settings.html")]
+pub struct Settings {
+    version: &'static str,
+    git: &'static str,
+
+    title: String,
+}
+
+#[derive(Template)]
+#[template(path = "dashboard/stats.html")]
+pub struct Stats {
+    version: &'static str,
+    git: &'static str,
+
+    title: String,
+}
+
+#[derive(Template)]
+#[template(path = "dashboard/Tasks.html")]
+pub struct Tasks {
+    version: &'static str,
+    git: &'static str,
+
+    title: String,
 }

9bec5be1dd171bde106d37325f50ae0935c0227e
Txuritan <txuritan@protonmail.com>
2020-07-23 19:17:44 -0400

    I always forget something..

--- stry-frontend/src/lib.rs
+++ stry-frontend/src/lib.rs
@@ -3,12 +3,30 @@ use {
     tokio::sync::broadcast::Receiver, warp::Filter,
 };
 
+// TODO: figure out how to have a conditional building for routes
 pub async fn start(cfg: Arc<Config>, mut rx: Receiver<()>, backend: DataBackend) {
     let state = warp::any().map(move || backend.clone()).boxed();
 
     let routes = akibisuto_stylus::route()
         .or(stry_frontend_api::route(state.clone()))
         .or(stry_frontend_user::route(state.clone()))
+        .with(warp::trace::request());
+
+    let (addr, server) = warp::serve(routes)
+        .bind_with_graceful_shutdown((cfg.host, cfg.port), async move {
+            rx.recv().await.expect("Failed to listen for event")
+        });
+
+    tracing::info!("warp drive engaged: listening on http://{}", addr);
+
+    server.await;
+}
+
+pub async fn start_api(cfg: Arc<Config>, mut rx: Receiver<()>, backend: DataBackend) {
+    let state = warp::any().map(move || backend.clone()).boxed();
+
+    let routes = akibisuto_stylus::route()
+        .or(stry_frontend_api::route(state.clone()))
         .with(warp::trace::request());
 
     let (addr, server) = warp::serve(routes)
